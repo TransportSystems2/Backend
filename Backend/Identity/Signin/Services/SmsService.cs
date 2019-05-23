a884ecf24e89665184808a6115a5ec3a4bbfe6df
fadeev <black_pol@mail.ru>
2019-05-23 23:16:08 +0300

    сервис для отправки смс использует токен авторизации из переменых окружения

--- Backend/Identity/Signin/Services/SmsService.cs
+++ Backend/Identity/Signin/Services/SmsService.cs
@@ -1,28 +1,32 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace TransportSystems.Backend.Identity.Signin.Services
 {
     public class SmsService : ISmsService
     {
-        private const string ApiId = "05C73C31-B3EE-B51C-1602-335148E17A9B";
-
-        public SmsService(ILogger<SmsService> logger)
+        private const string SmsEnv = "SMS_TOKEN";
+        public SmsService(ILogger<SmsService> logger, IConfiguration configuration)
         {
             Logger = logger;
+            Configuration = configuration;
         }
 
         private ILogger<SmsService> Logger { get; }
+
+        private IConfiguration Configuration { get; }
 
         public async Task<bool> SendAsync(string phoneNumber, string body)
         {
             var result = false;
             try
             {
+                var token = Configuration.GetValue<string>(SmsEnv);
                 var client = new HttpClient();
-                var query = $"https://sms.ru/sms/send?api_id={ApiId}&to={phoneNumber}&msg={body}&json=1";
+                var query = $"https://sms.ru/sms/send?api_id={token}&to={phoneNumber}&msg={body}&json=1";
                 await client.GetAsync(query);
 
                 Logger.LogInformation("Message was sended");
