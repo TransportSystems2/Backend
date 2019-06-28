bda7d95a277d91d2ab9c5f92d510e48a3652d719
Pave Fedorov <fedorkinp@yandex.ru>
2019-06-28 15:30:33 +0300

    resolved #180

---
+++ Backend/Identity/Signin/Services/ProfileService.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using TransportSystems.Backend.Identity.Core.Data.Domain;
+
+namespace TransportSystems.Backend.Identity.Signin.Services
+{
+    public class ProfileService : IProfileService
+    {
+        protected UserManager<User> _userManager;
+
+        public ProfileService(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            var user = await _userManager.GetUserAsync(context.Subject);
+
+            var claims = new List<Claim>
+            {
+                new Claim("CompanyId", user.CompanyId.ToString())
+            };
+
+            context.IssuedClaims.AddRange(claims);
+        }
+
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            var user = await _userManager.GetUserAsync(context.Subject);
+            context.IsActive = user != null;
+        }
+    }
+}
